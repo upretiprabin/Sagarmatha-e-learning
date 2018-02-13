845c6c20df03185148de549b67f3af8f267c3e66
Nitish Maharjan <nitish.atom@gmail.com>
2018-02-13 17:51:38 +0545

    Commit for initial deployment of live streaming and chat using tokbox. Signalling opentokbox demo.

---
+++ js/tokbox/config.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+
+// Make a copy of this file and save it as config.js (in the js directory).
+
+// Set this to the base URL of your sample server, such as 'https://your-app-name.herokuapp.com'.
+// Do not include the trailing slash. See the README for more information:
+
+var SAMPLE_SERVER_BASE_URL = 'http://YOUR-SERVER-URL';
+
+// OR, if you have not set up a web server that runs the learning-opentok-php code,
+// set these values to OpenTok API key, a valid session ID, and a token for the session.
+// For test purposes, you can obtain these from https://tokbox.com/account.
+
+var API_KEY = '46057212';
+var SESSION_ID = '2_MX40NjA1NzIxMn5-MTUxODUyMTk3OTQ2NH5wTnNXWUJoRlI4Rk9RTHg0RHhvNDcwZmp-fg';
+var TOKEN = 'T1==cGFydG5lcl9pZD00NjA1NzIxMiZzaWc9NzNlYzU2ZWI2YTRlNTFiNTdhY2I3OWJkYWVhYzhlMDYzYzg3MTUwZjpzZXNzaW9uX2lkPTJfTVg0ME5qQTFOekl4TW41LU1UVXhPRFV5TVRrM09UUTJOSDV3VG5OWFdVSm9SbEk0Ums5UlRIZzBSSGh2TkRjd1ptcC1mZyZjcmVhdGVfdGltZT0xNTE4NTIyMDE2Jm5vbmNlPTAuOTM3MTkyNDgyNTc2Njg0OSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNTIxMTE0MDE0JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9';
